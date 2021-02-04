4b7af5c8106bc4a4e8a8331f19c41890dc06dd71
ihch <ihachihiro@team-lab.com>
2021-02-04 23:14:51 +0900

    fix: mdxのコンポーネント変換の型定義

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,10 @@ import { MDXProvider } from '@mdx-js/react';
 import Doc from '../components/Doc.mdx';
 import styles from '../styles/Home.module.css'
 
+type IProps = {};
+
 const components = {
-  h1: props => <h1 style={{ color: 'teal', textAlign: 'left' }} {...props} />,
+  h1: (props: IProps) => <h1 style={{ color: 'teal', textAlign: 'left' }} {...props} />,
 };
 
 export default function Home() {
