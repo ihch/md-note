3d9e3e19d3d45de011e5d1d2d385e5d6bd096449
ihch <ryubi.102395@gmail.com>
2021-02-07 21:49:02 +0900

    fix: lint error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
 import Doc from '../components/Doc.mdx';
 import styles from '../styles/Home.module.css';
 
-type IProps = {};
+type IH1Props = {};
+const H1 = (props: IH1Props) => <h1 style={{ color: 'teal', textAlign: 'left' }} {...props} />;
 
 const components = {
-  h1: (props: IProps) => <h1 style={{ color: 'teal', textAlign: 'left' }} {...props} />,
+  h1: H1,
 };
 
 export default function Home() {
