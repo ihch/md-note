ba81499759a13f592caadbcd22c4d592ba41b8dc
ihch <ihachihiro@team-lab.com>
2021-02-08 23:11:40 +0900

    feat: テキストコンポーネントを追加した

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,14 @@ import { MDXProvider } from '@mdx-js/react';
 import Header from '../components/Header';
 import Doc from '../components/Doc.mdx';
 import { H1, H2, H3, IH1Props, IH2Props, IH3Props } from 'components/Heading';
+import { A, IAProps, IPProps, P } from 'components/Text';
 
 const components = {
   h1: (props: IH1Props) => <H1 {...props} />,
   h2: (props: IH2Props) => <H2 {...props} />,
   h3: (props: IH3Props) => <H3 {...props} />,
+  p: (props: IPProps) => <P {...props} />,
+  a: (props: IAProps) => <A {...props} />,
 };
 
 export default function Home() {
