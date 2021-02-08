0c2b9852ae28aebc6f6abf9f94f1d4228612afec
ihch <ihachihiro@team-lab.com>
2021-02-08 23:17:03 +0900

    fix: tag component mapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,15 @@ import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
 import Header from '../components/Header';
 import Doc from '../components/Doc.mdx';
-import { H1, H2, H3, IH1Props, IH2Props, IH3Props } from 'components/Heading';
-import { A, IAProps, IPProps, P } from 'components/Text';
+import { H1, H2, H3 } from 'components/Heading';
+import { A, P } from 'components/Text';
 
 const components = {
-  h1: (props: IH1Props) => <H1 {...props} />,
-  h2: (props: IH2Props) => <H2 {...props} />,
-  h3: (props: IH3Props) => <H3 {...props} />,
-  p: (props: IPProps) => <P {...props} />,
-  a: (props: IAProps) => <A {...props} />,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  p: P,
+  a: A,
 };
 
 export default function Home() {
