8d72fcca3fb96dd21204e183745684e342dc39ae
ihch <ihachihiro@team-lab.com>
2021-02-08 22:49:27 +0900

    feat: 見出しコンポーネントを作った

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
 import Header from '../components/Header';
 import Doc from '../components/Doc.mdx';
-
-type IH1Props = {};
-const H1 = (props: IH1Props) => <h1 style={{ color: 'teal', textAlign: 'left' }} {...props} />;
+import { H1, H2, H3, IH1Props, IH2Props, IH3Props } from 'components/Heading';
 
 const components = {
-  h1: H1,
+  h1: (props: IH1Props) => <H1 {...props} />,
+  h2: (props: IH2Props) => <H2 {...props} />,
+  h3: (props: IH3Props) => <H3 {...props} />,
 };
 
 export default function Home() {
@@ -21,9 +21,11 @@ export default function Home() {
 
       <Header title="ihch dev" />
 
-      <MDXProvider components={components}>
-        <Doc />
-      </MDXProvider>
+      <div className="px-16 py-8">
+        <MDXProvider components={components}>
+          <Doc />
+        </MDXProvider>
+      </div>
     </div>
   );
 }
