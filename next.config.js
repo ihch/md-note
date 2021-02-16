b53b0ada6547ca068779d290a77dce592c5ac546
ihch <ryubi.102395@gmail.com>
2021-02-16 22:27:50 +0900

    chore: github pagesからvercelに移行する

--- next.config.js
+++ next.config.js
@@ -4,5 +4,4 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/md-note/' : '',
 });
