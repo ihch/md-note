01247cee4702b4114950bd8e3e3570a738177fed
ihch <nemu0sou@gmail.com>
2022-07-08 15:14:54 +0900

    chore: update next, react, mdx-js, etc...

--- pages/articles/[name].tsx
+++ pages/articles/[name].tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { MDXProvider } from '@mdx-js/react';
 import { H1, H2, H3 } from 'components/Heading';
 import { A, P } from 'components/Text';
 import Articles from 'articles';
@@ -18,6 +16,7 @@ const Article: React.FC = () => {
   const router = useRouter();
   const { name } = router.query;
   const article = Articles.articles.find((article) => article.name === name);
+  const Post = article?.component;
 
   return (
     <div>
@@ -35,7 +34,9 @@ const Article: React.FC = () => {
       </Head>
 
       <div className="px-16 py-8">
-        <MDXProvider components={components}>{article && article.component({})}</MDXProvider>
+        {Post && (
+          <Post components={components} />
+        )}
       </div>
     </div>
   );
