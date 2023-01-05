c66bbec7dbba56d844368c4e357399bbf305ac5a
ihch <nemu0sou@gmail.com>
2023-01-05 18:12:24 +0900

    インプット大全を読んだ話

--- articles/index.tsx
+++ articles/index.tsx
@@ -1,4 +1,5 @@
 import MdNote from './md-note.mdx';
+import ReadThePowerOfInput from './read-the-power-of-input.mdx';
 
 export default {
   articles: [
@@ -7,6 +8,12 @@ export default {
       name: 'Next.jsでブログを作った',
       date: '2021/02/12',
       component: MdNote,
+    },
+    {
+      id: '0002',
+      name: '『インプット大全』を読んだ',
+      date: '2023/01/05',
+      component: ReadThePowerOfInput,
     },
   ],
 };
