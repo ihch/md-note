336b721b4d1b4896b14bd288c5661fb2c03b0f3c
ihch <nemu0sou@gmail.com>
2023-06-20 19:44:35 +0900

    feat: mdxをやめてmarkedで生成するようにした

--- articles/index.tsx
+++ articles/index.tsx
@@ -5,12 +5,14 @@ export default {
     articles: [
         {
             id: '0001',
+            articleName: 'md-note',
             name: 'Next.jsでブログを作った',
             date: '2021/02/12',
             component: MdNote,
         },
         {
             id: '0002',
+            articleName: 'read-the-power-of-input',
             name: '『インプット大全』を読んだ',
             date: '2023/01/05',
             component: ReadThePowerOfInput,
--- pages/articles/[name].tsx
+++ pages/articles/[name].tsx
@@ -1,42 +1,69 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { H1, H2, H3 } from 'components/Heading';
-import { A, P } from 'components/Text';
 import Articles from 'articles';
+import { marked, Renderer } from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
+import fs from 'fs';
+import { join } from 'path';
 
-const components = {
-    h1: H1,
-    h2: H2,
-    h3: H3,
-    p: P,
-    a: A,
-};
-
-const Article: React.FC = () => {
-    const router = useRouter();
-    const { name } = router.query;
-    const article = Articles.articles.find((article) => article.name === name);
-    const Post = article?.component;
-
+const Article = ({ name, md }: { name: string, md: string }) => {
     return (
         <div>
             <Head>
-                <title>{article?.name}</title>
+                <title>{name}</title>
                 <meta name="twitter:card" content="summary" />
                 <meta name="twitter:site" content="@nemu_sou" />
                 <meta name="twitter:creator" content="@nemu_sou" />
                 <meta
                     property="og:url"
-                    content={`${process.env.BUILD_URL}/articles/${article?.name}`}
+                    content={`${process.env.BUILD_URL}/articles/${name}`}
                 />
-                <meta property="og:title" content={article?.name} />
+                <meta property="og:title" content={name} />
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"
+                />
                 {/* TODO <meta property="og:image" content="" /> */}
             </Head>
 
-            <div className="px-16 py-8">{Post && <Post components={components} />}</div>
+            <div className="px-16 py-8">
+                <div
+                    className="markdown-body"
+                    dangerouslySetInnerHTML={{
+                        __html: md,
+                    }}
+                />
+            </div>
         </div>
     );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const name = context.params?.name || null;
+    const article = Articles.articles.find((article) => article.name === name);
+    const md = article?.articleName ? fs.readFileSync(join('articles', `${article.articleName}.mdx`), { encoding: 'utf8' }) : '';
+
+    const renderer = new Renderer();
+    renderer.code = (code, lang: string) => {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        // @ts-ignore
+        const highlited = hljs.highlight(code, { language, theme: 'onedark' }).value;
+        return `<pre><code class='hljs ${lang}'>${highlited}</code></pre>`;
+    };
+
+    return {
+        props: {
+            name,
+            md: marked(md, {
+                renderer,
+                gfm: true,
+                mangle: false,
+                headerIds: false,
+            }),
+        },
+    };
 };
 
 export default Article;
